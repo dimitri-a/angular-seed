fb47054e0bf062aa39b0d1890252febb9621f2c1
Dimitri Couwenberg <couwenberg@gmail.com>
2017-12-21 14:25:16 +1100

    added myservice

---
+++ app/services/myservice.js
@@ -0,0 +1,5 @@
+app.service('helloUsingService', function() {
+    this.sayHello = function() {
+        return 'Hello from the service';
+    }
+});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,10 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [ 'helloUsingService',function(helloUsingService) {
+
+    //todo remove
+    debugger;
+    console.log(helloUsingService.sayHello());
 
 }]);

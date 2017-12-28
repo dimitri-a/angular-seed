a42f237bc00629d8635069ef333713347e30092a
Dimitri <couwenberg@gmail.com>
2017-12-29 08:59:06 +1100

    unit test view1 ctrl  injected

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,14 +2,22 @@
 
 describe('myApp.view1 module', function() {
 
+  var $scope,$rootScope;
+
   beforeEach(module('myApp.view1'));
+
+  beforeEach(inject(function(_$rootScope_){
+    $rootScope =_$rootScope_;
+    $scope =_$rootScope_.$new();
+  }));
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should be defined and stuff', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var view1Ctrl = $controller('View1Ctrl',{$scope:$scope});
       expect(view1Ctrl).toBeDefined();
+      //expect($scope.greeting).toEqual('hello');
     }));
 
   });

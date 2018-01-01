d07bbbd271a1d1434e17cdfd4df8a4032e494df7
Dimitri <couwenberg@gmail.com>
2018-01-01 20:13:07 +1100

    unit test 4 of 4 working, view 1 app running

--- app/services/DataService.js
+++ app/services/DataService.js
@@ -1,9 +1,9 @@
-'use strict';
-
-console.log('hello dataservice');
-angular.module('myApp.service', [])
-    .service('doStuff', function($http) {
-        this.update = function(data) {
-            return $http.get('https://jsonplaceholder.typicode.com/todos', data);
-        }
-    })
+// 'use strict';
+//
+// console.log('hello dataservice');
+// angular.module('myApp.service', [])
+//     .service('doStuff', function($http) {
+//         this.update = function(data) {
+//             return $http.get('https://jsonplaceholder.typicode.com/todos', data);
+//         }
+//     })

e1b37993c6aa5b2dede8d077924051b9fac9ab07
Dimitri <couwenberg@gmail.com>
2018-01-02 13:10:57 +1100

    update

--- app/view2/view2_test_mock_service.js
+++ app/view2/view2_test_mock_service.js
@@ -15,22 +15,28 @@ describe('Testing a Controller that uses a Mocked out service', function () {
 
 
     //trying to test whether the dataservice.getGreeting is called
-    it('should resolve promise and mock the DataService', inject(function ($q,$controller,$rootScope) {
+    it('should resolve promise and mock the DataService', inject(function ($q, $controller, $rootScope) {
         //create promise
         deferred = $q.defer();
 
         //mock service response (foc controller's constructor)
+        //dont care what it returns so deferred.promise is a mocked return
         mockService.getGreeting.and.returnValue(deferred.promise);
 
+        //initialize controller and scope
         localScope = $rootScope.$new();
-        ctrl = $controller('view2Ctrl', {$scope:localScope ,DataService: mockService});
+        ctrl = $controller('view2Ctrl', {$scope: localScope, DataService: mockService});
 
+        //call init
         localScope.init();
 
+        //return mock value for promise
         deferred.resolve(1);
 
+        //run digest cycle to get to 'then' part
         $rootScope.$apply();
 
+        //the scope variable length is assigned the value of the promise resolve
         expect(localScope.length).toBe(1);
     }));
 

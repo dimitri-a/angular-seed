7c61ac9b66eda33798167f1e768033259a29256c
Dimitri <couwenberg@gmail.com>
2018-01-06 20:13:19 +1100

    start list of promises

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -8,69 +8,20 @@ app
         });
     }])
 
-    .controller('view3Ctrl', function ($q, $timeout, myService) {
+
+    //controller that generates promises and displays status
+    .controller('view3Ctrl', function ($q) {
 
 
-        var one = $q.defer();
-        var two = $q.defer();
-        var three = $q.defer();
 
 
-        var all = $q.all([one.promise, two.promise, three.promise]);
-
-        all.then(function (res) {
-            console.log('hier');
-            console.log(res);
-        });
-
-
-        one.resolve('one is resolved');
-        two.resolve('two is resolved');
-        three.resolve('three is resolved');
 
 
     })
-    .factory('myService', function ($timeout) {
+    .factory('generator',function(){
         return {
-            go: function () {
-                return $timeout(function () {
-                    return 'hello from service';
-                }, 1000);
+            giveProm:function(){
+                return $q.defer();
             }
         }
     });
-
-
-// 'use strict';
-//
-// app
-//     .config(['$routeProvider', function ($routeProvider) {
-//         $routeProvider.when('/view3', {
-//             templateUrl: 'view3/view3.html',
-//             controller: 'view3Ctrl'
-//         });
-//     }])
-//
-//     .controller('view3Ctrl', function ($q, myService) {
-//         //create q service:
-//         var defer = $q.defer();
-//
-//         // call async
-//         myService.go().then(
-//             function () {
-//                 console.log(result);
-//             }
-//         );
-//     })
-//     .factory('myService', function ($timeout) {
-//         return {
-//             go: function ( $q) {
-//                 $timeout(function () {
-//                     $q.resolve('hello');
-//                 }, 1000);
-//             }
-//         }
-//     });
-//
-//
-//
